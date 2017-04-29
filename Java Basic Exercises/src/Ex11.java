fa987f19387326fda0283147524be07a0b58eceb
Verwolff <verwolff1@gmail.com>
2017-04-30 00:59:34 +0100

    11 and 12

---
+++ Java Basic Exercises/src/Ex11.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Ex11 {
+	public static void main(String[] args){
+		Scanner sc;
+		sc = new Scanner(System.in);
+		System.out.println("Please provide radius of circle");
+		double radius = sc.nextDouble();
+		double perimeter = 2*Math.PI*radius;
+		double area = Math.PI*radius*radius;
+		System.out.println("Permieter of circle is "+perimeter);
+		System.out.println("Area of circle is "+area);
+	}
+}
