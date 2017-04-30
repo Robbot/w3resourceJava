a3900cc0b3c612da8a0df3f2739f8f73d1e2509b
Verwolff <verwolff1@gmail.com>
2017-04-30 10:27:13 +0100

    a few new solved and new folder

---
+++ Data Types/w3resourceJava/Ex01.java
@@ -0,0 +1,13 @@
+package w3resourceJava;
+import java.util.*;
+public class Ex01 {
+	public static void main(String[] args){
+		Scanner sc;
+		sc = new Scanner(System.in);
+		System.out.println("Please provide temperature in Fahrenheit");
+		double fahrenheit = sc.nextDouble();
+		double  celsius =(( 5 *(fahrenheit - 32.0)) / 9.0);
+        System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celsius + " in Celsius");
+	}
+
+}
