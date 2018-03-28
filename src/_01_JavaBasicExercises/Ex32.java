e005eac591b34b346172b119d73b59e29d535645
Verwolff <verwolff1@gmail.com>
2018-03-28 20:18:25 +0100

    milena

---
+++ src/_01_JavaBasicExercises/Ex32.java
@@ -0,0 +1,26 @@
+package _01_JavaBasicExercises;
+//Write a Java program to compare two numbers
+import java.util.Scanner;
+public class Ex32{
+public static void main(String[] args) {
+	int first_num,second_num;
+	Scanner in = new Scanner (System.in);
+	System.out.print("Input a first number: ");
+	first_num = in.nextInt();
+	System.out.print("Input a second number: ");
+	second_num = in.nextInt();
+	if ( first_num == second_num )           
+        System.out.printf( "%d == %d\n", first_num, second_num );  
+    if ( first_num != second_num )          
+        System.out.printf( "%d != %d\n", first_num, second_num );  
+    if ( first_num < second_num )          
+        System.out.printf( "%d < %d\n", first_num, second_num );  
+    if ( first_num > second_num )          
+        System.out.printf( "%d > %d\n", first_num, second_num );  
+    if ( first_num <= second_num )          
+        System.out.printf( "%d <= %d\n", first_num, second_num );  
+    if ( first_num >= second_num )          
+        System.out.printf( "%d >= %d\n", first_num, second_num ); 
+	
+	}
+}
--- src/_02_DataTypes/Ex01.java
+++ src/_02_DataTypes/Ex01.java
@@ -1,13 +1,15 @@
 package _02_DataTypes;
 import java.util.*;
+import java.lang.Math;
 public class Ex01 {
 	public static void main(String[] args){
 		Scanner sc;
 		sc = new Scanner(System.in);
 		System.out.println("Please provide temperature in Fahrenheit");
 		double fahrenheit = sc.nextDouble();
-		double  celsius =(( 5 *(fahrenheit - 32.0)) / 9.0);
-        System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celsius + " in Celsius");
+		double celsius =(( 5 *(fahrenheit - 32.0)) / 9.0);
+		long rounded = Math.round(celsius,2);
+        System.out.println(fahrenheit + " degree Fahrenheit is equal to " + rounded + " in Celsius");
 	}
 
 }
